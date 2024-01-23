a851c26a80044e09d4d9911c32115d6d1ad78b8f
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2024-01-23 15:31:43 -0300

    Localstorage: getItem and setItem

--- src/pages/Movies/index.js
+++ src/pages/Movies/index.js
@@ -39,6 +39,25 @@ function Movies() {
     };
   }, [navigate, id]);
 
+  function saveMovie() {
+    const myList = localStorage.getItem("@rockerflix");
+
+    let savedMovies = JSON.parse(myList) || [];
+
+    const hasMovie = savedMovies.some(
+      (savedMovie) => savedMovie.id === movie.id
+    );
+
+    if (hasMovie) {
+      alert("Esse filme já esta na lista");
+      return;
+    }
+
+    savedMovies.push(movie);
+    localStorage.setItem("@rockerflix", JSON.stringify(savedMovies));
+    alert("Filme salvo com sucesso!");
+  }
+
   if (loading) {
     return (
       <div className="filme-info">
@@ -59,7 +78,7 @@ function Movies() {
       <strong>Avaliação: {movie.vote_average} /10</strong>
 
       <div className="area-buttons">
-        <button>Salvar</button>
+        <button onClick={saveMovie}>Salvar</button>
         <button>
           <a
             href={`https://youtube.com/results?search_query=${movie.title} Trailer`}
